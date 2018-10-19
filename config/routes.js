a3752a248693680c65adf5ad570bab8380960483
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-19 11:17:23 -0400

    Huthman King: Sprint - Authentication Start of the project User is able to register Upon completion of registration, user is presented a token

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const knex = require ('knex');
+
 
 const { authenticate } = require('./middlewares');
+
+const dbConfig = require('../knexfile');
+const db = knex(dbConfig.development);
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +15,36 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const jwtSecret = 'This is a secret, shhh!';
+  const jwtPayload = {
+    username: user.username,
+    
+  }
+  const jwtOptions = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
+
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  if ( !creds || !creds.username || !creds.password) {
+    res.status(400).json({
+      message: "Both fields are required."
+    })
+  } else {
+    db('users')
+      .insert(creds)
+      .then( user => {
+        const token = generateToken(user);
+        res.status(201).json({ id: user.id, token });
+      })
+      .catch( err => { res.status(500).json( err.message )});
+  }
 }
 
 function login(req, res) {
